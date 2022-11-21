b6d4ae4d78068234681fb326735a092abfcbd6ac
kerenmevorach <mevok873@newschool.edu>
2022-11-21 00:46:44 -0500

    fixed responsive nav

--- entry-1/script.js
+++ entry-1/script.js
@@ -4,65 +4,54 @@ let navText = document.querySelector('.nav-text')
 let toggle = document.querySelector('#toggle')
 let body = document.querySelector('body')
 
+function smallMobileNav(){
+  mobileTitle.style.fontSize = "calc(var(--base)/1.3)";
+  mobileTitle.style.lineHeight = "calc(var(--leading)/1.9)";
+  mobileTitle.style.fontWeight = "200";
+  mobileTitle.style.marginTop = "4px";
+  firstEntry.style.height = "var(--nav-height)";
+}
 
+function largeMobileNav(){
+  mobileTitle.style.fontSize = "35px";
+  mobileTitle.style.lineHeight = "40px";
+  mobileTitle.style.fontWeight = "100";
+  mobileTitle.style.marginTop = "8px";
+  firstEntry.style.height = "150px";
+}
+
+function desktopNav(){
+  mobileTitle.style.fontSize = "20px";
+  mobileTitle.style.lineHeight = "calc(var(--leading)/1.9)";
+  mobileTitle.style.fontWeight = "200";
+  mobileTitle.style.marginTop = "4px";
+  firstEntry.style.height = "100%";
+}
 
 
 function scrollFunction() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 0) {
-    mobileTitle.style.fontSize = "calc(var(--base)/1.3)";
-    mobileTitle.style.lineHeight = "calc(var(--leading)/1.9)";
-    mobileTitle.style.fontWeight = "200";
-    mobileTitle.style.marginTop = "4px";
-    firstEntry.style.height = "var(--nav-height)";
+    smallMobileNav()
   } else {
-    mobileTitle.style.fontSize = "35px";
-    mobileTitle.style.lineHeight = "40px";
-    mobileTitle.style.fontWeight = "100";
-    mobileTitle.style.marginTop = "8px";
-    firstEntry.style.height = "150px";
-}
+    largeMobileNav()
+  }
   }
   
 
   function scrollToggle(x) {
     if (x.matches) { 
       // scrollFunction()
+      console.log('add')
       window.addEventListener("scroll", scrollFunction);
-      // toggle.classList.remove("desktop-title");
-      // toggle.classList.add("mobile-title");
+      largeMobileNav()
     } else {
+      console.log('remove')
       window.removeEventListener("scroll",scrollFunction);
-      // toggle.classList.remove("mobile-title");
-      // toggle.classList.add("desktop-title");
+      desktopNav()
     }
   }
-
-  //   window.addEventListener("load", (event) => {
-  //   console.log("page is fully loaded");
-  // });
 
   
   let x = window.matchMedia("(max-width: 750px)")
   scrollToggle(x) // Call listener function at run time
   x.addListener(scrollToggle) // Attach listener function on state changes
-
-  // window.addEventListener("resize", scrollToggle);
-
-  // body.onload = function(){
-  //   console.log('hi there')
-  //   // scrollFunction
-  // };
-
-
-  // window.addEventListener("load", (event) => {
-  //   console.log("page is fully loaded");
-  //   scrollToggle(x)
-  // });
-
-
-  //on load make font this
-
-
-  //if body or if viewport width =. etc...
-
-  
